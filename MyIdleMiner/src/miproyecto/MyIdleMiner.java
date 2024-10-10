4f773eca0500a953396af198962ce9742ba8d9e7
frama <frama@WIN-NRHO900L6V5>
2024-10-10 17:12:29 +0200

    Test exitoso de constructores. Closes #5

--- MyIdleMiner/src/miproyecto/MyIdleMiner.java
+++ MyIdleMiner/src/miproyecto/MyIdleMiner.java
@@ -16,8 +16,8 @@ public class Oro {
 
     public static final String ICONO = "/img/oro.png";
     public static final int DURACION = 2;
-    public static final int EXPERIENCIA = 1;
-    public static final int PRECIO_VENTA = 2;
+    public static final int EXPERIENCIA = 10;
+    public static final int PRECIO_VENTA = 20;
     public static final int MINAR = 10;         
     
     private String icono;
@@ -42,15 +42,40 @@ public class Oro {
     public Oro(){
         
     }
-    
+ 
     public Oro(Oro otro){
         
         this(otro.icono, otro.duracionProduccion, otro.experienciaRecolecta, otro.precioVenta, otro.minar);
     }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Oro{");
+            sb.append("icono=").append(icono);
+            sb.append(", duracionProduccion=").append(duracionProduccion);
+            sb.append(", experienciaRecolecta=").append(experienciaRecolecta);
+            sb.append(", precioVenta=").append(precioVenta);
+            sb.append(", minar=").append(minar);
+            sb.append('}');
+            return sb.toString();
+        }
     
     
-    
-    
+    }
+
+    public static void main(String[] args){
+        
+        MyIdleMiner idleMiner = new MyIdleMiner();
+        
+        Oro o1 = idleMiner.new Oro();
+        Oro o2 = idleMiner.new Oro(Oro.ICONO, 3, 10, 20, 1);
+        Oro o3 = idleMiner.new Oro(o1);
+        
+        System.out.println(o1);
+        System.out.println(o2);
+        System.out.println(o3);
+        
     }
 
 }
