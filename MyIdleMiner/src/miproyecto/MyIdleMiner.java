b7bb3f736acc31a421c7fe843d965dcfa440d58c
frama <frama@WIN-NRHO900L6V5>
2024-10-17 16:14:02 +0200

    Clase diamante. Closes #9

--- MyIdleMiner/src/miproyecto/MyIdleMiner.java
+++ MyIdleMiner/src/miproyecto/MyIdleMiner.java
@@ -21,8 +21,8 @@ public class Oro {
 
     public static final String ICONO = "/img/oro.png";
     public static final int DURACION = 2;
-    public static final int EXPERIENCIA = 10;
-    public static final int PRECIO_VENTA = 20;
+    public static final int EXPERIENCIA = 1;
+    public static final int PRECIO_VENTA = 2;
     public static final int MINAR = 10;         
     
     private String icono;
@@ -31,7 +31,7 @@ public class Oro {
     private int precioVenta;
     private int minar;
     private TipoMinar tipo;
-
+    
 
 
 //Constructores
@@ -127,17 +127,135 @@ public class Oro {
     
     }
 
+
+
+    
+public class Diamante {
+    
+    public static final String ICONO = "/img/diamante.png";
+    public static final int DURACION = 5;
+    public static final int EXPERIENCIA = 2;
+    public static final int PRECIO_VENTA = 3;
+    public static final int MINAR = 20;
+    
+    private String icono;
+    private int duracionProduccion;
+    private int experienciaRecolecta;
+    private int precioVenta;
+    private int minar;
+    private TipoMinar tipo;
+    
+    
+    //Constructores
+    
+    
+    public Diamante(String icono, int duracionProduccion, int experienciaRecolecta, int precioVenta, int minar){
+        
+        this.icono = icono;
+        this.duracionProduccion = duracionProduccion;
+        this.experienciaRecolecta = experienciaRecolecta;
+        this.precioVenta = precioVenta;
+        this.minar = minar;
+    }
+    
+    public Diamante(){
+        
+    }
+    
+    public Diamante(Diamante otro){
+        
+        this(otro.icono, otro.duracionProduccion, otro.experienciaRecolecta, otro.precioVenta, otro.minar);
+    }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Diamante{");
+            sb.append("icono=").append(icono);
+            sb.append(", duracionProduccion=").append(duracionProduccion);
+            sb.append(", experienciaRecolecta=").append(experienciaRecolecta);
+            sb.append(", precioVenta=").append(precioVenta);
+            sb.append(", minar=").append(minar);
+            sb.append(", tipo=").append(tipo);
+            sb.append('}');
+            return sb.toString();
+        }
+    
+    public void setIcono(String icono){
+        
+        this.icono = icono;
+    }
+    
+    public String getIcono(){
+        
+        return icono;
+    }
+    
+    public void setduracionProduccion(int duracionProduccion){
+        
+        this.duracionProduccion = duracionProduccion;
+    }
+    
+    public int getduracionProduccion(){
+        
+        return duracionProduccion;
+    }
+    
+    public void setexperienciaRecolecta(int experienciaRecolecta){
+        
+        this.experienciaRecolecta = experienciaRecolecta;
+    }
+    
+    public int getexperienciaRecolecta(){
+        
+        return experienciaRecolecta;
+    }
+    
+    public void setprecioVenta(int precioVenta){
+        
+        this.precioVenta = precioVenta;
+    }
+    
+    public int getprecioVenta(){
+        
+        return precioVenta;
+    }
+    
+    public void setminar(int minar){
+        
+        this.minar = minar;
+    }
+    
+    public int getminar(){
+        
+        return minar;
+    }
+    
+    public TipoMinar getTipo(){
+        
+        return tipo;
+    }
+    
+}
+
     public static void main(String[] args){
         
         MyIdleMiner idleMiner = new MyIdleMiner();
         
         Oro o1 = idleMiner.new Oro();
-        Oro o2 = idleMiner.new Oro(Oro.ICONO, 5, 10, 20, 1);
+        Oro o2 = idleMiner.new Oro(Oro.ICONO, 2, 1, 2, 10);
         Oro o3 = idleMiner.new Oro(o1);
+        Diamante d1 = idleMiner.new Diamante();
+        Diamante d2 = idleMiner.new Diamante(Diamante.ICONO, 5, 2, 3, 20);
+        Diamante d3 = idleMiner.new Diamante(d1);
         
         System.out.println(o1);
         System.out.println(o2);
         System.out.println(o3);
+        
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
         
         System.out.println("Precio: " + o1.getprecioVenta());
         System.out.println("Duracion de la produccion: " + o1.getduracionProduccion());
@@ -145,17 +263,39 @@ public class Oro {
         System.out.println("Icono: " + o1.getIcono());
         System.out.println("Minar: " + o1.getminar());
         
-        o1.setprecioVenta(5);
+        System.out.println("Precio: " + d1.getprecioVenta());
+        System.out.println("Duracion de la produccion: " + d1.getduracionProduccion());
+        System.out.println("Exp: " + d1.getexperienciaRecolecta());
+        System.out.println("Icono: " + d1.getIcono());
+        System.out.println("Minar: " + d1.getminar());
+        
+        o1.setprecioVenta(2);
         System.out.println("Precio: " + o1.getprecioVenta());
         
-        o1.setduracionProduccion(10);
+        d1.setprecioVenta(3);
+        System.out.println("Precio: " + d1.getprecioVenta());
+        
+        o1.setduracionProduccion(2);
         System.out.println("Duracion de la produccion: " + o1.getduracionProduccion());
         
-        o1.setexperienciaRecolecta(20);
+        d1.setduracionProduccion(5);
+        System.out.println("Duracion de la produccion: " + d1.getduracionProduccion());
+        
+        o1.setexperienciaRecolecta(1);
         System.out.println("Exp: " + o1.getexperienciaRecolecta());
+        
+        d1.setexperienciaRecolecta(2);
+        System.out.println("Exp: " + d1.getexperienciaRecolecta());
+        
+        o1.setminar(10);
+        System.out.println("Min: " + o1.getminar());
+        
+        d1.setminar(20);
+        System.out.println("Min: " + d1.getminar());
         
         System.out.println(o1);
         
-    }
+    }       
+
 
 }
